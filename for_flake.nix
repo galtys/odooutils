282c2dc1c49d64c7d9c9526e9cf95bbfeb075f39
Jan Troler <jan.troler@galtys.com>
2026-04-20 14:22:45 +0200

    PyPDF and PyPDF2 at the same time

--- for_flake.nix
+++ for_flake.nix
@@ -452,7 +452,7 @@ let
       };
       #python27.pkgs.six python27.pkgs.suds-jurko
       checkInputs = [ pkgs.glibcLocales ];
-      buildInputs = [ my_pillow ]; #pkgs.python27.pkgs.pillow ];
+      buildInputs = [ my_pillow my_pypdf]; #pkgs.python27.pkgs.pillow ];
       #buildInputs = [ pkgs.python27.pkgs.pillow ];
       #buildInputs = [ pkgs.python27.pkgs.ft pkgs.python27.pkgs.pillow ];
 
@@ -738,9 +738,9 @@ let
    #my_requests_oauthlib
     pkgs27=pkgs.python27Packages;
     #my_schwifty
-    my_py_packages2=with pkgs.python27Packages; [ certifi urllib3 my_unidecode pbr my_twilio  chardet pycups phonenumbers  my_psycopg2 tabulate my_openupgradelib my_pillow  ldap my_pywebdav my_vatnumber gdata Babel chardet decorator docutils feedparser gevent greenlet html2text my_jinja2  lxml Mako markupsafe mock  ofxparse passlib  psutil  pydot  pyparsing my_pypdf2 pyserial python-dateutil pytz pyusb qrcode my_reportlab requests suds-jurko vobject my_werkzeug xlwt xlrd simplejson pychart unittest2 pycountry numpy wheel unicodecsv  matplotlib my_magento my_pyyaml311 my_openid setuptools my_helpscout  gspread  oauth2client toml my_odooutils pandas singledispatch my_oauthlib ]++(my-apiclient pkgs27); 
+    my_py_packages2=with pkgs.python27Packages; [ certifi urllib3 my_unidecode pbr my_twilio  chardet pycups phonenumbers  my_psycopg2 tabulate my_openupgradelib my_pillow  ldap my_pywebdav my_vatnumber gdata Babel chardet decorator docutils feedparser gevent greenlet html2text my_jinja2  lxml Mako markupsafe mock  ofxparse passlib  psutil  pydot  pyparsing my_pypdf my_pypdf2 pyserial python-dateutil pytz pyusb qrcode my_reportlab requests suds-jurko vobject my_werkzeug xlwt xlrd simplejson pychart unittest2 pycountry numpy wheel unicodecsv  matplotlib my_magento my_pyyaml311 my_openid setuptools my_helpscout  gspread  oauth2client toml my_odooutils pandas singledispatch my_oauthlib ]++(my-apiclient pkgs27); 
     
-   my_py_packages3=with pkgs.python27Packages; [my_psycopg2 tabulate my_openupgradelib pillow  ldap my_pywebdav my_vatnumber gdata Babel chardet decorator docutils feedparser gevent greenlet html2text my_jinja2  lxml Mako markupsafe mock  ofxparse passlib  psutil  pydot  pyparsing my_pypdf2 pyserial python-dateutil pytz pyusb qrcode my_reportlab requests suds-jurko vobject my_werkzeug xlwt xlrd simplejson pychart unittest2 pycountry numpy unicodecsv  matplotlib my_magento my_pyyaml311 my_openid setuptools my_helpscout  gspread  oauth2client toml my_odooutils pandas singledispatch]; #before v8
+   my_py_packages3=with pkgs.python27Packages; [my_psycopg2 tabulate my_openupgradelib pillow  ldap my_pywebdav my_vatnumber gdata Babel chardet decorator docutils feedparser gevent greenlet html2text my_jinja2  lxml Mako markupsafe mock  ofxparse passlib  psutil  pydot  pyparsing my_pypdf my_pypdf2 pyserial python-dateutil pytz pyusb qrcode my_reportlab requests suds-jurko vobject my_werkzeug xlwt xlrd simplejson pychart unittest2 pycountry numpy unicodecsv  matplotlib my_magento my_pyyaml311 my_openid setuptools my_helpscout  gspread  oauth2client toml my_odooutils pandas singledispatch]; #before v8
 
   mypython = pkgs.python27.buildEnv.override {
      extraLibs =  my_py_packages2;  
